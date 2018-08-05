45687215725f433d2bb6b3cb26c84c356d063eda
Hope Dunner <hpdnnr7@gmail.com>
2018-08-05 07:23:23 +0000

    Done.

--- crazy_string.rb
+++ crazy_string.rb
@@ -1,5 +1,9 @@
 # write your method here
 
-def crazy_strings(a,b)
+def crazy_strings(apple, pears)
+  apple = apple.reverse.upcase
 
+    pears = pears.swapcase.gsub("s", "z")
+
+    return "#{apple} #{pears}"
 end
